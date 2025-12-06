ec82eb090bd68c2743b82966d3a47cbb827c845b
sakis070864 <mastorematas@gmail.com>
2025-12-07 00:35:06 +0100

    Added auto-scroll and highlight for duplicate invoices

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Rythmisi tou build target se es2020 gia na ypostirizetai to import.meta.env
+  build: {
+    target: 'es2020'
+  },
+  esbuild: {
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
 })
